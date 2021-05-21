aae12c795aef0a12f31270e04ff5839989f900d3
root <you@example.com>
2021-05-21 10:19:31 +0530

    hi

--- checkPath.py
+++ checkPath.py
@@ -1,1 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+import os
+from os import path
+#my_file = open("dirlist.txt", "r")
+#content = my_file.read()
+#print(content)
+
+my_file = open("dirlist.txt", "r")
+content_list = my_file.readlines()
+for i in content_list:
+    print(i)
+    print("Hello world")
+
