cfa73fc0d34a617f2a6bec981c6056ec78b58987
root <you@example.com>
2021-05-21 16:14:55 +0530

    python

--- checkPath.py
+++ checkPath.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 import os
 from os import path
+import pathlib
+import re
+import shutil
+
 #my_file = open("dirlist.txt", "r")
-#content = my_file.read()
-#print(content)
+#content_list = my_file.readlines()
 
-my_file = open("dirlist.txt", "r")
-content_list = my_file.readlines()
-for i in content_list:
-    print(i)
-
+with open('dirlist.txt') as content_list:
+    for i in content_list:
+        if path.exists(i):
+            print("File exist")
+        else:
+            #print(i)
+            #os.makedirs(i)
+            print("File not exist")
