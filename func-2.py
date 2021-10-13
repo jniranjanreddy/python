ae9fd5ab40ec1e28837319761fdcf56e8fe2e343
niru <you@example.com>
2021-10-12 21:22:40 -0400

    kl

--- func-2.py
+++ func-2.py
@@ -1,10 +1,15 @@
-#!/usr/bin/env python3
-
-def printme( str ):
-   "This prints a passed string into this function"
-   print(str)
-   return;
+#!/usr/bin/env python
+import os
+#def printme( str ):
+#   "This prints a passed string into this function"
+#   print(str)
+#   return;
 
 #printme("I'm first call to user defined function!")
 #printme("Again second call to the same function")
-printme()
+#printme()
+
+def Clear():
+    cls = ("clear")
+    os.system(cls)
+Clear()
