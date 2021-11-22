0bddc12de95600a4f13d996b09e4a12033ba9842
niru <you@example.com>
2021-11-22 09:50:08 -0500

    lk

--- modules/modules.py
+++ modules/modules.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
-import os
-print(help(os))
+def f1():
+    print("F1 Execution")
+
+def f2():
+    print("F2 Execution")
+
+def f3():
+    print("F3 Execution")
+if __name__ == '__main__':
+    f1()
+    f2()
+    f3()
---
+++ modules/test.py
@@ -0,0 +1,3 @@
+##!/usr/bin/env python3
+import modules as m
+print("hello", m.f2())
