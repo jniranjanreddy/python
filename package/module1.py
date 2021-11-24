4870403004c940e04456a57e98a2e4e37bc5a4a5
niru <you@example.com>
2021-11-24 12:42:13 -0500

    lk

---
+++ package/module1.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+def f1():
+    print("This is from f1 function present in the pack1 dir anf file module1.py")
---
+++ package/test.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+import pack.module1
+pack.module1.f1()
