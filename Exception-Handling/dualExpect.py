18f680ac9a4491802107bd0cc1efe64ac8bc1082
niru <you@example.com>
2022-01-12 21:47:09 -0500

    iu

---
+++ Exception-Handling/dualExpect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+try: 
+    x = int(input("Enter First Number: "))
+    y = int(input("Enter Second Number: "))
+    print("The result is: ", x/y)
+#except ZeroDivisionError as msg:
+#    print("Cannot Divide with Zero")
+#    print("The exception error: ", msg.__class__)
+#except ValueError as msg:
+#    print("Please provide int values only")
+#    print("The exception error: ", msg.__class__.__name__)
+#========================================================
+except (ZeroDivisionError,ValueError) as msg:
+    print("Please provide value input..")
+     
+    
