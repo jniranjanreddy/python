d2cd06350cc55ce0f7a3e2bb60f8cf04633c714c
niru <you@example.com>
2022-01-17 07:42:24 -0500

    oi

--- boto3/find-keypairs.py
+++ boto3/find-keypairs.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import boto3
 
-ec2 = boto3.client('ec2')
-response = ec2.create_key_pair(KeyName='boto-key')
-print(response)
+#ec2 = boto3.client('ec2')
+#response = ec2.create_key_pair(KeyName='boto-key')
+#print(response)
+ec2 = boto3.resource('ec2')
+instance = ec2.Instance('id')
+print(ec2)
