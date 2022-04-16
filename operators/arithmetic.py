aa2e3c23a9228e48637f6f7c55082d375c2e498a
SG-Niranjan-Jannapureddy <you@example.com>
2022-04-16 06:54:50 -0400

    gffffffffff

--- operators/arithmetic.py
+++ operators/arithmetic.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+#Arithmetic operators "+ - * / % // **"
+#//
+a = 10
+b = 20
+c = a
+# print(a>b)
+# print(a<b)
+# print(id(a))
+# print(id(b))
+# print(id(c))
+# print(a is b)
+# print(a is c)
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+print("--------------------------------------")
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+
---
+++ operators/logical_operator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Logical Operator
+
+# userName = input("Enter user name: ")
+# passWord = input("Enter password: ")
+# if userName == "niru" and passWord == "mypass":
+#     print("Hey!! I am Valid user")
+# else:
+#     print("Invalid user")
+print(10 or 20)
