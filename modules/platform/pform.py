7a8906207adb0f17036a5ece36437ef2b68cb85b
admin <admin@nirulabs.com>
2022-10-26 09:09:16 -0400

    er

--- modules/platform/pform.py
+++ modules/platform/pform.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-<<<<<<< HEAD
 import platform as p
 print(platform.system())
 print(platform.node())
@@ -15,19 +14,3 @@ print(platform.architecture())
 print(platform.processor())
 print(platform.machine())
 print(platform.machine())
-=======
-import platform
-print(platform.system().lower())
-# print(platform.node())
-# print(platform.platform())
-# print(platform.release())
-# print(platform.uname())
-# # print(platform.system_alias())
-# print(platform.python_version_tuple())
-# print(platform.machine())
-# print(platform.release())
-# print(platform.architecture())
-# print(platform.python_version_tuple())
-# print(platform.processor())
-# print(platform.machine())
->>>>>>> 8eda4bd9b463f33e41c0c791f12dca6e52199a08
