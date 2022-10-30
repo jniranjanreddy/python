1757cd28e2b92460d8df80e84f9b6ba0ecbe0914
admin <admin@nirulabs.com>
2022-10-30 05:53:34 -0400

    ew

--- modules/re/example.py
+++ modules/re/example.py
@@ -8,8 +8,23 @@ findall  re.findall(pattern,text)
 split
 sub ..etc
 """
-my_str = "This is simple and it is easy"
-# my_pattern = "i[st]"
-my_pattern = "ab"
+# my_str = "This is simple and b it is python"
+# my_pattern = "i[st]"    # It will find for is and it
+# my_pattern = "[abcd]"    #  It will print all characters between a to d
+# my_pattern = "[a-d]"    # It will print all characters between a to d.
+# print(len(re.findall(my_pattern,my_str)))
+# my_pattern = "\w"
+# my_pattern = "\w\w"
+# my_pattern = "\w\w\w"  Prints all characters belons to \w, a to z and _
+# my_pattern = "\W"      #Matches all characters with doesnt belons to small w (\w) _ space
+my_str = "This is simple and b it is python _ # - ?"
+my_pattern = "\W"
 print(re.findall(my_pattern,my_str))
-print(len(re.findall(my_pattern,my_str)))
+
+
+
+
+
+
+
+
