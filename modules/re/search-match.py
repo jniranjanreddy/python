1c5034b9902e365446cbd6755937ce9b921d9d4d
admin <admin@nirulabs.com>
2022-11-01 02:27:38 -0400

    wq

--- modules/re/search-match.py
+++ modules/re/search-match.py
@@ -2,4 +2,6 @@ import re
 
 text = "This is for python there ate two major version python2 and python3, in future for python4"
 pat = r"\bpython[\d]?\b"
-print(re.search(pat,text))
+match_ob = re.search(pat,text)
+# my_search = re.findall(pat,text)
+print(match_ob)
