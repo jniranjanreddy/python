d3bedded5bb0829de205789bddd764854bc70c5c
admin <admin@nirulabs.com>
2022-11-19 09:11:53 -0500

    21

--- test-modules.py
+++ test-modules.py
@@ -1,3 +1,4 @@
 # import awsBoto3.localstack.createDeleteS3buckets as awsbuckets
-import awsBoto3.localstack.
-awsbuckets.create_bucket("test06")
+import awsBoto3.localstack.listS3Func as count
+# awsbuckets.create_bucket("test06")
+count.s3_count()
