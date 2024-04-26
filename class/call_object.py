905979bc0853ddc98f912ea46d92758e50093cd7
Your Name <you@example.com>
2024-04-26 23:49:05 +0530

    wq

---
+++ class/call_object.py
@@ -0,0 +1,6 @@
+class MyClass:
+    def __init__(self):
+        print("Initializing MyClass")
+
+# Creating an instance of MyClass
+hello = MyClass()  # Output: "Initializing MyClass"
--- class/example-1.py
+++ class/example-1.py
@@ -7,6 +7,7 @@ class Dog:
         self.name = "USA"
         self.state = "California"
         print(f"{self.name} says woof!")
+        print(f"{self.state} says woof!")
         
 # Creating an instance of the Dog class
 my_dog = Dog("Buddy", 3)
@@ -14,6 +15,6 @@ my_dog = Dog("Buddy", 3)
 # Accessing attributes and methods of the Dog class
 print(my_dog.name)  # Output: Buddy
 print(my_dog.age)   # Output: 3
-# my_dog.bark()       # Output: Buddy says woof!
-print(my_dog.state) # Output: California
-print(my_dog.name)  # Output: USA
+my_dog.bark()       # Output: Buddy says woof!
+# print(my_dog.bark.state) # Output: California
+# print(my_dog.name)  # Output: USA
