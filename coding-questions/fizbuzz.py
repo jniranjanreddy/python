716aa686d79deb3abb16d193e7f49d8fd1925143
Your Name <you@example.com>
2024-05-24 11:52:42 +0530

    added coding

--- coding-questions/fizbuzz.py
+++ coding-questions/fizbuzz.py
@@ -1,11 +1,16 @@
-for i in range(1,16+1):
-    if i % 3 == 0 and i % 5 == 0:
-        print("fizbuz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("buzz")
-    else:
-        print(i)
+# for i in range(1,16+1):
+#     if i % 3 == 0 and i % 5 == 0:
+#         print("fizbuz")
+#     elif i % 3 == 0:
+#         print("Fizz")
+#     elif i % 5 == 0:
+#         print("buzz")
+#     else:
+#         print(i)
     
 
+a = 10
+b = 20
+print(a,b)
+a,b = b,a
+print(a,b)
