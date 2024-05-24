c34d9c76d958e4a3619b6fcfbf218aece316984b
Your Name <you@example.com>
2024-05-24 20:17:14 +0530

    wq

---
+++ coding-questions/palindrome-4.py
@@ -0,0 +1,30 @@
+# # Reverse
+# a = [1,2,3,4]
+# print(a[::-1])
+
+b = -123
+c = str(b)
+# print(c[::-0])
+# print(c[::-1])
+print(c[::2])
+print(c[::3])
+print(c[::4])
+print(c[::5])
+print(c[::-2])
+print(c[::-3])
+print(c[::-4])
+
+
+# number = 123
+# string_number = str(number)
+# print(type(string_number))
+
+################
+# def myPolindrome(x):
+#     string_number = str(x)
+#     if string_number == string_number[::-1]:
+#         print("Its polindrome")
+#     else:
+#         print("Its not palindrome")
+
+# myPolindrome(20002)
