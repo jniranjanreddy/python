8068aae5698ee44c326d71eee774d4456fcc85ee
Your Name <you@example.com>
2024-05-26 15:17:17 +0530

    wq

---
+++ coding-questions/Lists_EX/compare_find_unique.py
@@ -0,0 +1,9 @@
+a = [1, 2, 3, 4, 5, 6,7,8,9]
+
+b = [1, 2, 4, 5, 6, 7,10]
+
+# print(set(a) - set(b)) # sets can be subtracted from each other
+# uniQue = list(set(a) - set(b)) # This is the same as the line below
+
+uniQue = list(set(a).difference(b))
+print(uniQue) # [8, 9, 3] 3 is the missing number
--- coding-questions/Maths_EX/find_missing_number.py
+++ coding-questions/Maths_EX/find_missing_number.py
@@ -6,9 +6,7 @@ def find_missing(input_list):
   n = len(input_list) + 1
   actual_sum = (n * ( n + 1 ) ) / 2
  
-  return int(actual_sum - sum_of_elements)
-list_1 = [1,5,6,3,4]
-
-
+  # return int(actual_sum - sum_of_elements)
+  print(int(actual_sum - sum_of_elements))
+list_1 = [1,3,4,5,]
 find_missing(list_1)
-print(n)
